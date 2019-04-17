08eb966e57ccc19f1a89f45058f92d8e9f637b98
yuguang <yuguang@analyticsmd.com>
2019-04-17 10:21:01 -0700

    add todo notes

--- ssm.py
+++ ssm.py
@@ -1,7 +1,10 @@
 from qventus_conf import fetch_secrets
 
+# TODO: modify function to take in sqla.engine.url. See https://superset.incubator.apache.org/installation.html#external-password-store-for-sqlalchemy-connections
+
 def ssm_password_lookup(url):
   environment = 'prod'
+  # TODO: generate stack and secret from sqla.engine.url
   stack = 'stanford'
   secret = '/amd-db/superset/password'
   secrets = [secret, ]
