fc5bc4a32611446f97ba1b73bf9fdf423728c193
niharikayadav <niharika.yadav7@gmail.com>
2019-11-26 16:12:53 -0800

    Removing credentials

--- contrib/docker/install-dependency.sh
+++ contrib/docker/install-dependency.sh
@@ -11,9 +11,10 @@ systemctl start mysqld
 yum install -y git
 mkdir /home/superset
 cd /home/superset
-git clone https://niharikay@github.com/analyticsMD/incubator-superset.git
+git clone https://github.com/analyticsMD/incubator-superset.git
 pip install docker-compose
-docker login -uqventusci -p3FvK7kN?]Lie4
-docker pull qventus/superset:0.31.3
+docker login -uqventusci -p
+docker pull qventus/superset:0.31.4
 cd /home/superset/incubator-superset
 git checkout qventus-release-0.31.2
+cd contrib/docker
